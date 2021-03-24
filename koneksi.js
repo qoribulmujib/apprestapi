dfedaac8ea927777d33fe95986314373bef6abae
qoribul mujib <muhamadqoribulmujib@gmail.com>
2021-03-25 00:11:34 +0700

    nambahin koneksi ke database mysql

---
+++ koneksi.js
@@ -0,0 +1,17 @@
+//panggil librarry mysql
+var mysql = require("mysql");
+
+//buat koneksi database
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dbrestapi'
+});
+
+conn.connect((error) => {
+    if(error) throw error;
+    console.log("mysql terkoneksi");
+});
+
+module.exports = conn;
