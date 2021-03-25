6b0aecacf7b4dadf3b3d577ef65fd69adfb3cdbb
qoribul mujib <muhamadqoribulmujib@gmail.com>
2021-03-25 10:21:06 +0700

    berhasil menampilkan semua data

--- controller.js
+++ controller.js
@@ -5,4 +5,15 @@ var connection = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("aplikasi berjalan lancar", res);
-}
+};
+
+//menampilkan semua data mahasiswa
+ exports.tampilSemuaData = function(req, res){
+     connection.query("SELECT * FROM mahasiswa", function(error, rows, fields){
+         if(error){
+             connection.log(error);
+         }else {
+             response.ok(rows, res);
+         }
+     });
+ };
