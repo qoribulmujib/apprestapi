117c167e40c253d395042298ad1def6caa901666
qoribul mujib <muhamadqoribulmujib@gmail.com>
2021-03-28 12:13:53 +0700

    user berhasil login bawa token

--- middleware/auth.js
+++ middleware/auth.js
@@ -40,3 +40,52 @@ exports.registrasi = function(req, res){
         }
     });
 };
+
+//buat controller untuk login
+exports.loggin = function(req, res){
+    let post = {
+        email : req.body.email,
+        password : req.body.password
+    }
+
+    let query = "SELECT * FROM ??  WHERE ?? = ? AND ?? = ?";
+    let table = ["users", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query, table);
+    connection.query(query, function(error, rows){
+        if(error){
+            console.log(error);
+        }else{
+            if(rows.length == 1){
+            let token = jwt.sign({rows}, config.secret, {
+                expiresIn: 1440
+            });
+            idUser = rows[0].id;
+            let data = {
+                idUser: idUser,
+                aksesToken: token,
+                ipAddress: ip.address()
+            }    
+            //query unutuk insert
+            let query = "INSERT INTO ?? SET ?";
+            let table = ["aksestoken"];
+            
+            query = mysql.format(query, table);
+            connection.query(query, data, function(error, rows){
+                if(error){
+                    console.log(error);
+                }else {
+                     res.json({
+                         success: true,
+                         massage: 'token JWT tergenerate!',
+                         token: token,
+                         currUser: data.idUser
+                     });
+                }
+            });
+            }else{
+                res.json({'error': true, 'massage': 'email atau password salah'});
+            }
+        }
+    });
+}
--- middleware/index.js
+++ middleware/index.js
@@ -5,4 +5,7 @@ var router = express.Router();
 //menmabahkan menu registrasi 
 router.post('/api/v1/register', auth.registrasi);
 
+//menambahkan menu login
+router.post('/api/v1/login', auth.loggin);
+
 module.exports = router;
