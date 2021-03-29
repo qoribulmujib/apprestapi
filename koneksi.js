61a5cf1a980690a01646cfd1400c098fb65d09fb
qoribul mujib <muhamadqoribulmujib@gmail.com>
2021-03-29 16:11:35 +0700

    beres

--- koneksi.js
+++ koneksi.js
@@ -3,15 +3,15 @@ var mysql = require("mysql");
 
 //buat koneksi database
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'dbrestapi'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "dbrestapi",
 });
 
 conn.connect((error) => {
-    if(error) throw error;
-    console.log("mysql terkoneksi");
+  if (error) throw error;
+  console.log("mysql terkoneksi");
 });
 
 module.exports = conn;
--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -1,29 +1,37 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config/secret');
+const jwt = require("jsonwebtoken");
+const config = require("../config/secret");
 
 //verifikasi role
-function verifikasi(roles){
-    return function(req, rest, next){
-        //cek authorization
-        let tokenWithBearer = req.headers.authorization;
-        if (tokenWithBearer){
-            let token = tokenWithBearer.split(' ')[1];
-            //verifikasi
-            jwt.verify(token, config.secret, function(error, decoded){
-                if ( error){
-                    return rest.status(401).send({auth:false, message: 'token tidak terdaftar'});
-                }else{
-                    if (roles == 2){
-                        req.auth = decoded;
-                        next();
-                    }else {
-                        return rest.status(401).send({auth:false, message: 'gagal mengotorization role anda'});
-                    }
-                }
+function verifikasi() {
+  return function (req, rest, next) {
+    let role = req.body.role;
+    //cek authorization
+    let tokenWithBearer = req.headers.authorization;
+    if (tokenWithBearer) {
+      let token = tokenWithBearer.split(" ")[1];
+      //verifikasi
+      jwt.verify(token, config.secret, function (error, decoded) {
+        if (error) {
+          return rest
+            .status(401)
+            .send({ auth: false, message: "token tidak terdaftar" });
+        } else {
+          if (role == 2) {
+            req.auth = decoded;
+            next();
+          } else {
+            return rest.status(401).send({
+              auth: false,
+              message: "gagal mengotorization role anda",
             });
-        }else {
-            return rest.status(401).send({auth:false, message: 'token tidak tersedia'});
+          }
         }
-    }    
+      });
+    } else {
+      return rest
+        .status(401)
+        .send({ auth: false, message: "token tidak tersedia" });
+    }
+  };
 }
 module.exports = verifikasi;
